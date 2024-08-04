cdd58421e3e9e46161452abee70739be3a27108a
Ivan Lins <ivanlins73@gmamil.com>
2024-08-04 17:42:40 -0300

    Atribuindo valores recuperados e exibindo

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,10 +1,18 @@
 function sortear(){
-    let quantidadeNumeros = document.getElementById("quantidade").value; //atribuindo valor de 'quantidade' à uma variável
-    let numeroMinimo = parseInt(document.getElementById("de").value); //atribuindo valor de 'de' à uma variável
-    let numeroMaximo = parseInt(document.getElementById("ate").value); //atribuindo valor de 'ate' à uma variável
+    let quantidadeNumeros = document.getElementById("quantidade").value; //alocando valor de 'quantidade' à uma variável
+    let numeroMinimo = parseInt(document.getElementById("de").value); //alocando valor de 'de' à uma variável
+    let numeroMaximo = parseInt(document.getElementById("ate").value); //alocando valor de 'ate' à uma variável
 
-    alert(`qtd numeros: ${quantidadeNumeros}`); //teste para saber se os números estão sendo recuperados corretamente
-    alert(`de: ${numeroMinimo}`); //teste para saber se os números estão sendo recuperados corretamente
-    alert(`ate: ${numeroMaximo}`); //teste para saber se os números estão sendo recuperados corretamente
+    let numerosSorteados = []; //declarando as variaveis
+    let numero; //declarando as variaveis
 
+    for ( let i = 0; i < quantidadeNumeros; i++){ //Faça isso X vezes, e depois pare
+        numero = obterNumeroRandom(numeroMinimo, numeroMaximo); //alocando os valores da função de numeros aleatorios
+        numerosSorteados.push(numero); //alocando os valores obtidos na lista
+    }
+    alert(numerosSorteados); //exibindo a lista de numeros sorteados
+}
+
+function obterNumeroRandom(min, max){ //obtendo números aleatorios
+    return Math.floor(Math.random() * (max - min + 1)) + min; //retornando os valores para os parametros
 }
