98a7ee3b5ee0217345dfeab96467ff4954d15f91
Ivan Lins <ivanlins73@gmamil.com>
2024-08-04 16:57:43 -0300

    Recuperando dados do documento

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,3 +1,10 @@
 function sortear(){
+    let quantidadeNumeros = document.getElementById("quantidade").value; //atribuindo valor de 'quantidade' à uma variável
+    let numeroMinimo = parseInt(document.getElementById("de").value); //atribuindo valor de 'de' à uma variável
+    let numeroMaximo = parseInt(document.getElementById("ate").value); //atribuindo valor de 'ate' à uma variável
+
+    alert(`qtd numeros: ${quantidadeNumeros}`); //teste para saber se os números estão sendo recuperados corretamente
+    alert(`de: ${numeroMinimo}`); //teste para saber se os números estão sendo recuperados corretamente
+    alert(`ate: ${numeroMaximo}`); //teste para saber se os números estão sendo recuperados corretamente
 
 }
