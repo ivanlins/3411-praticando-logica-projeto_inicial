4f385f4259971fe1a9196ab6d07fb3fb1889d369
Ivan Lins <ivanlins73@gmamil.com>
2024-08-06 21:13:32 -0300

    Desafio carrinho concluido

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,19 +1,53 @@
-let carrinhoDeCompras; //variavel para armazenar a lista de compras.
-let valoresProdutos; //variavel para armazenar a lista preços.
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$0';
 
-function adicionar(){ //função para quando for clicado em "adicionar" recuperar quais produtos foram adicionados
-    
+
+function adicionar() { //função para quando for clicado em "adicionar" recuperar quais produtos foram adicionados
+    let carrinhoDeCompras; //variavel para armazenar a lista de compras.
+    let valoresProdutos; //variavel para armazenar a lista preços.
+    let valorTotal; //variavel para armazenar elemento de total do HTML     
     let quantidadeProdutos = document.getElementById('quantidade').value; //recuperando a quantidade de produtos
     let produtos = document.getElementById('produto').value; //recuperando o produto selecionado
-    carrinhoDeCompras = produtos.split('-')[0];
-    valoresProdutos = parseFloat(produtos.split('$')[1]).toFixed(2);
+    let listaCarrinho = document.getElementById('lista-produtos');
+    let valorFinal = document.getElementById('valor-total'); //recuperando o elemento de valor total do documento
 
-    if(quantidadeProdutos == ''){ //condicional para verificar se a quantidade é 0
+
+
+    carrinhoDeCompras = produtos.split('-')[0];
+    valoresProdutos = parseFloat(produtos.split('$')[1]);
+
+    if (carrinhoDeCompras == '') { //condicional para verificar se a quantidade é 0
+        alert('insita um produto valido');
+        return;
+    }
+
+
+    if (quantidadeProdutos == '') { //condicional para verificar se a quantidade é 0
+        alert('insita uma quantidade valida');
+        return;
         quantidadeProdutos = 1;
     }
 
-    console.log(quantidadeProdutos); //verificando no console se a quantidade foi recuperada corretamente
-    console.log(carrinhoDeCompras);
-    console.log(valoresProdutos);
+    //alterar valores dentro do carrinho do documento
+    valorTotal = parseFloat(quantidadeProdutos * valoresProdutos); //calculando o valor total do produto
 
+    listaCarrinho.innerHTML = listaCarrinho.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidadeProdutos}x</span> ${carrinhoDeCompras} <span class="texto-azul">R$${valorTotal}</span></section>`;
+    totalGeral = totalGeral + valorTotal;
+
+    valorFinal.textContent = (`R$${totalGeral}`); //imprimindo valor final na tela
+
+    quantidadeProdutos = '';
+    document.getElementById('quantidade').value = null;
+
+}
+
+function limpar() {
+    totalGeral = 0;
+    valorTotal = 0;
+    carrinhoDeCompras = null;
+    listaCarrinho = null;
+
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
 }
